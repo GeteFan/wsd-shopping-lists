37eb49673630cccff92a2c35a6b2f92ab16706d3
root <root@DESKTOP-AU15A0S>
2023-04-19 16:02:10 +0300

    small changes

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -1,10 +1,13 @@
 import { postgres } from "../deps.js";
 
+Deno.env.set("DATABASE_URL", "postgres://roman_moroz_wsd_shopping_lists_user:t8ZYqT9dsf76hHt9Ek9p80MzXhZaqcWV@dpg-cgvsskpeuhlhlbhpjp70-a/roman_moroz_wsd_shopping_lists");
+
 let sql;
 if (Deno.env.get("DATABASE_URL")) {
   sql = postgres(Deno.env.get("DATABASE_URL"));
-} else {
-  sql = postgres({});
-}
+} 
+// else {
+//   sql = postgres({});
+// }
 
 export { sql };
