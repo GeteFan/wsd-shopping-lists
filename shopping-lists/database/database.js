f11c756a343b3e737ad7fc1120d92320b93f6b69
root <root@DESKTOP-AU15A0S>
2023-04-27 15:33:36 +0300

    copied code for views lists

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -1,11 +1,10 @@
 import { postgres } from "../deps.js";
+import * as listService from "../services/listService.js";
 
 Deno.env.set("DATABASE_URL", "postgres://roman_moroz_wsd_shopping_lists_user:t8ZYqT9dsf76hHt9Ek9p80MzXhZaqcWV@dpg-cgvsskpeuhlhlbhpjp70-a/roman_moroz_wsd_shopping_lists");
 let sql;
 if (Deno.env.get("DATABASE_URL")) {
   sql = postgres(Deno.env.get("DATABASE_URL"));
-  await sql`INSERT INTO shopping-lists (name) VALUES ("name")`;
-  console.log(await sql`SELECT * FROM shopping_lists WHERE active = true`);
 } else {
   console.log("not adfaf");
   sql = postgres({});
