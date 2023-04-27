df033bf3e415b62d13675eddd0955cb924c51ca6
root <root@DESKTOP-AU15A0S>
2023-04-27 14:55:11 +0300

    changes in docker-compose

--- shopping-lists/app.js
+++ shopping-lists/app.js
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
-import { configure } from "https://deno.land/x/eta@v2.0.0/mod.ts";
+import { configure, renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
 import * as listController from "./controllers/listController.js";
 
 configure({
--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -1,10 +1,12 @@
 import { postgres } from "../deps.js";
 
+Deno.env.set("DATABASE_URL", "postgres://roman_moroz_wsd_shopping_lists_user:t8ZYqT9dsf76hHt9Ek9p80MzXhZaqcWV@dpg-cgvsskpeuhlhlbhpjp70-a/roman_moroz_wsd_shopping_lists");
 let sql;
 if (Deno.env.get("DATABASE_URL")) {
+  console.log("adfaf");
   sql = postgres(Deno.env.get("DATABASE_URL"));
-} 
-else {
+} else {
+  console.log("not adfaf");
   sql = postgres({});
 }
 
