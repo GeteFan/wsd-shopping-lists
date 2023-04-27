a8f8bad55128f3125380774d26ac6ca4af741691
root <root@DESKTOP-AU15A0S>
2023-04-27 15:18:12 +0300

    add some console logs to test more precisely

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -3,8 +3,9 @@ import { postgres } from "../deps.js";
 Deno.env.set("DATABASE_URL", "postgres://roman_moroz_wsd_shopping_lists_user:t8ZYqT9dsf76hHt9Ek9p80MzXhZaqcWV@dpg-cgvsskpeuhlhlbhpjp70-a/roman_moroz_wsd_shopping_lists");
 let sql;
 if (Deno.env.get("DATABASE_URL")) {
-  console.log("adfaf");
   sql = postgres(Deno.env.get("DATABASE_URL"));
+  await sql`INSERT INTO shopping-lists (name) VALUES ("name")`;
+  return await sql`SELECT * FROM shopping_lists WHERE active = true`;
 } else {
   console.log("not adfaf");
   sql = postgres({});
