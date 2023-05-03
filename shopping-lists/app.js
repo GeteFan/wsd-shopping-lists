83e8383aad28ba10d5010085657d1228f10912c9
root <root@DESKTOP-AU15A0S>
2023-05-03 13:47:23 +0300

    changed if cases so they not overlap

--- shopping-lists/app.js
+++ shopping-lists/app.js
@@ -20,10 +20,10 @@ const handleRequest = async (request) => {
     return await itemController.viewListItems(request);
   } else if (url.pathname.match("lists/[0-9]+") && request.method === "POST") {
     return await itemController.addItem(request);
-  } else if (url.pathname.match("lists/[0-9]+/[0-9]+") && request.method === "POST") {
-    return await itemController.addItem(request);
   } else if (url.pathname.match("lists/[0-9]+/[0-9]+/collect") && request.method === "POST") {
     return await itemController.collectItem(request);
+  } else if (url.pathname.match("lists/[0-9]+/[0-9]+") && request.method === "POST") {
+    return await itemController.addItem(request);
   } else {
     return new Response("Not found", { status: 404 });
   }
