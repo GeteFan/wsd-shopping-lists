1672a68da2ee321fc77615bdc2fc31d37bc4661d
root <root@DESKTOP-AU15A0S>
2023-05-05 15:43:43 +0300

    add responseDetails

--- shopping-lists/controllers/listController.js
+++ shopping-lists/controllers/listController.js
@@ -1,6 +1,5 @@
 import { renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
 import * as listService from "../services/listService.js";
-import * as itemService from "../services/itemService.js";
 import * as requestUtils from "../utils/requestUtils.js";
 
 const responseDetails = {
--- shopping-lists/controllers/mainController.js
+++ shopping-lists/controllers/mainController.js
@@ -3,6 +3,10 @@ import * as listService from "../services/listService.js";
 import * as itemService from "../services/itemService.js";
 import * as requestUtils from "../utils/requestUtils.js";
 
+const responseDetails = {
+    headers: { "Content-Type": "text/html;charset=UTF-8" },
+};
+
 const viewMain = async (request) => {
     const data = {
       shopping_lists: await listService.countAllLists(),
