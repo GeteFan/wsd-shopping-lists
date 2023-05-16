6b8090e430234254d31a60a82d184b8d92498370
root <root@DESKTOP-AU15A0S>
2023-05-16 14:46:35 +0300

    add connection Pool(4)

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -1,24 +1,27 @@
 import { postgres } from "../deps.js";
 import { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+// import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+
+// const client = new Client();
 
 Deno.env.set("DATABASE_URL", "postgres://roman_moroz_wsd_shopping_lists_user:t8ZYqT9dsf76hHt9Ek9p80MzXhZaqcWV@dpg-cgvsskpeuhlhlbhpjp70-a/roman_moroz_wsd_shopping_lists");
 
 const env = postgres(Deno.env.get("DATABASE_URL"));
 
-const databaseCredentials = async (env) => {
-  let sql;
-  if (env) {
-    sql = env;
-  } else {
-    console.log("not adfaf");
-    sql = {};
-  }
-  return sql;
-};
+// const databaseCredentials = async (env) => {
+//   let sql;
+//   if (env) {
+//     sql = env;
+//   } else {
+//     console.log("not adfaf");
+//     sql = {};
+//   }
+//   return sql;
+// };
 
 
 const CONCURRENT_CONNECTIONS = 2;
-const connectionPool = new Pool(databaseCredentials(env), CONCURRENT_CONNECTIONS);
+const connectionPool = new Pool({}, CONCURRENT_CONNECTIONS);
 
 const executeQuery = async (query, params) => {
   const response = {};
