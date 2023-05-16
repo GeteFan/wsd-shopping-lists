fa4e739c9811e7b50fe5f1c450404c52dbbd7eb2
root <root@DESKTOP-AU15A0S>
2023-05-16 14:51:38 +0300

    add connection Pool(5)

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -6,7 +6,7 @@ import { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
 Deno.env.set("DATABASE_URL", "postgres://roman_moroz_wsd_shopping_lists_user:t8ZYqT9dsf76hHt9Ek9p80MzXhZaqcWV@dpg-cgvsskpeuhlhlbhpjp70-a/roman_moroz_wsd_shopping_lists");
 
-const env = postgres(Deno.env.get("DATABASE_URL"));
+const env = Deno.env.get("DATABASE_URL");
 
 // const databaseCredentials = async (env) => {
 //   let sql;
@@ -21,7 +21,9 @@ const env = postgres(Deno.env.get("DATABASE_URL"));
 
 
 const CONCURRENT_CONNECTIONS = 2;
-const connectionPool = new Pool({}, CONCURRENT_CONNECTIONS);
+const connectionPool = new Pool({
+  connectionString: env,
+}, CONCURRENT_CONNECTIONS);
 
 const executeQuery = async (query, params) => {
   const response = {};
