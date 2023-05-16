16c085ba165284ff90e8285ee5d0195dc0cfc838
root <root@DESKTOP-AU15A0S>
2023-05-16 16:02:54 +0300

    add connection Pool(14)

--- shopping-lists/services/listService.js
+++ shopping-lists/services/listService.js
@@ -18,7 +18,7 @@ const findAllActiveLists = async () => {
 
 const countAllLists = async () => {
   const number = await executeQuery("SELECT COUNT(*) FROM shopping_lists;");
-  console.log(number.rows[0]);
+  console.log(number.rows[0].count);
   if (number.rows[0].count == undefined || number.rows[0].count < 1) {
     return "No shopping lists yet.";
   } else {
